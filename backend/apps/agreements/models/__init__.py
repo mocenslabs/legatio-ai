79ccd30c7b5010b126a6fc6e28d607b90b38c691
Mauro Vicens <blackeagledhs@gmail.com>
2026-09-01 11:00:47 -0300

    feat(agreements): add Agreement model with status lifecycle

---
+++ backend/apps/agreements/models/__init__.py
@@ -0,0 +1,8 @@
+"""Agreements models.
+
+This module exports all models from the agreements app.
+"""
+
+from apps.agreements.models.agreement import Agreement, AgreementStatus
+
+__all__ = ["Agreement", "AgreementStatus"]

eca17608c52d4b90afc09b54e3bf3c6db3f9b2cc
Mauro Vicens <blackeagledhs@gmail.com>
2026-09-02 11:07:16 -0300

    feat(negotiations): add Negotiation and NegotiationOffer models

--- backend/apps/negotiations/models/__init__.py
+++ backend/apps/negotiations/models/__init__.py
@@ -4,5 +4,14 @@ This module exports all models from the negotiations app.
 """
 
 from apps.negotiations.models.comment import Comment, CommentEntityType
+from apps.negotiations.models.negotiation import Negotiation, NegotiationStatus
+from apps.negotiations.models.negotiation_offer import NegotiationOffer, OfferStatus
 
-__all__ = ["Comment", "CommentEntityType"]
+__all__ = [
+    "Comment",
+    "CommentEntityType",
+    "Negotiation",
+    "NegotiationOffer",
+    "NegotiationStatus",
+    "OfferStatus",
+]
